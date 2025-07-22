20580ca300ff4f179c0702073df7c471a3ff6177
RuchithaMannam <ruchithamannam123@gmail.com>
2025-07-23 00:01:57 +0530

    Adding project files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Counter App</h1>
+      <h6>{`Count: ${count}`}</h6>
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+export default App;
